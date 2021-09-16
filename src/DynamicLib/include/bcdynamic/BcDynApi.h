4f6aa6d4cd0e815fea1e3ca81c1af3bca96e9a33
Burak Can <bc913dev@outlook.com>
2021-09-15 21:46:35 -0400

    Update shared library export header for clang & gcc

--- src/DynamicLib/include/bcdynamic/BcDynApi.h
+++ src/DynamicLib/include/bcdynamic/BcDynApi.h
@@ -2,13 +2,17 @@
 #define BCDYNAPI_H
 
 #ifdef _WIN32
-# ifdef DYNLIB_EXPORT
-#   define BCDYN_API  __declspec( dllexport )
-# else
-#   define BCDYN_API  __declspec( dllimport )
-# endif
+#   ifndef BCDYN_API
+#       ifdef DYNLIB_EXPORT
+#           define BCDYN_API  __declspec( dllexport )
+#       else
+#           define BCDYN_API  __declspec( dllimport )
+#       endif
+#   endif
 #else
-#   define BCDYN_API
+#   ifndef BCDYN_API
+#       define BCDYN_API __attribute__((visibility("default")))
+#   endif
 #endif
 
-#endif
+#endif //BCDYNAPI_H
