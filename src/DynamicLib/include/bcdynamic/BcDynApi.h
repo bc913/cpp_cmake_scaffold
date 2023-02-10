eb50da05b48fd98dbeca5d03f32e6705d645bb93
Burak Can <bc913dev@outlook.com>
2023-02-09 22:19:16 -0500

    Use another style of if defined macro

--- src/DynamicLib/include/bcdynamic/BcDynApi.h
+++ src/DynamicLib/include/bcdynamic/BcDynApi.h
@@ -1,18 +1,35 @@
 #ifndef BCDYNAPI_H
 #define BCDYNAPI_H
 
-#ifdef _WIN32
-#   ifndef BCDYN_API
-#       ifdef DYNLIB_EXPORT
-#           define BCDYN_API  __declspec( dllexport )
-#       else
-#           define BCDYN_API  __declspec( dllimport )
-#       endif
-#   endif
-#else
-#   ifndef BCDYN_API
-#       define BCDYN_API __attribute__((visibility("default")))
-#   endif
+// #ifdef _WIN32
+// #   ifndef BCDYN_API
+// #       ifdef DYNLIB_EXPORT
+// #           define BCDYN_API  __declspec( dllexport )
+// #       else
+// #           define BCDYN_API  __declspec( dllimport )
+// #       endif
+// #   endif
+// #else
+// #   ifndef BCDYN_API
+// #       define BCDYN_API __attribute__((visibility("default")))
+// #   endif
+// #endif
+
+
+#if !defined(BCDYN_API)
+    #if defined(_MSC_VER)
+        #if defined(DYNLIB_EXPORT)
+            #define BCDYN_API  __declspec( dllexport )
+        #else
+            #define BCDYN_API  __declspec( dllimport )
+        #endif
+    #else
+        #if defined(DYNLIB_EXPORT)
+            #define BCDYN_API __attribute__((visibility("default"))) 
+        #else
+            #define BCDYN_API
+        #endif
+    #endif
 #endif
 
 #endif //BCDYNAPI_H
